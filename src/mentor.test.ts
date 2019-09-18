6df8717fd4ae55a4c3378b4eb5b80444a310b43a
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-09-18 20:48:06 +0200

    Only post a comment once

--- src/mentor.test.ts
+++ src/mentor.test.ts
@@ -1,11 +1,22 @@
 import { default as mentor } from './mentor'
 
+const mockApi = () => {
+  return {
+    issues: {
+      createComment: jest.fn(),
+      listComments: jest.fn()
+    }
+  };
+}
+
+const sampleEvent = { number: 1337, repository: { name: 'repo', owner: { login: 'owner'} } };
+
 describe('#mentor', () => {
   it('posts a comment to the github pr', async () => {
-    const api = { issues: { createComment: jest.fn() } };
-    const event = { number: 1337, repository: { name: 'repo', owner: { login: 'owner'} } };
+    const api = mockApi();
+    api.issues.listComments.mockReturnValueOnce([]);
 
-    await mentor(api, event);
+    await mentor(api, sampleEvent);
 
     expect(api.issues.createComment).toHaveBeenCalledWith({
       owner: 'owner',
@@ -13,6 +24,24 @@ describe('#mentor', () => {
       issue_number: 1337,
       body: expect.stringContaining("mentor"),
     });
+  });
+
+  it('doesnt comment a second time if there is already a mentor comment', async () => {
+    const api = mockApi();
+    api.issues.listComments.mockReturnValueOnce([{ body: 'by mentor', user: { login: 'github-action' } }]);
+
+    await mentor(api, sampleEvent);
+
+    expect(api.issues.createComment).not.toHaveBeenCalled();
+  });
+
+  it('posts if none of the comment is by mentor', async () => {
+    const api = mockApi();
+    api.issues.listComments.mockReturnValueOnce([{ body: 'whatever', user: { login: 'someone' } }]);
+
+    await mentor(api, sampleEvent);
+
+    expect(api.issues.createComment).toHaveBeenCalled();
   });
 
   it('comments with a mentor tip', async () => {
