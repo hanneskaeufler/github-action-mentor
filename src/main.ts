b321e082733f10813fc263893d5fe88a7b6c1fbb
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-09-18 08:39:20 +0200

    Try reading the pr details

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import * as fs from 'fs';
 
 async function run() {
   const api = new github.GitHub(core.getInput('githubToken', { required: true }));
-  const event = process.env['GITHUB_EVENT_PATH'];
+  const event = process.env['GITHUB_EVENT_PATH'] || '';
 
   console.log(event);
-  console.log(api.pulls.get());
+
+  const file = fs.readFileSync(event);
+  console.log(file);
+
+  console.log(api.pulls.get({
+    owner: '',
+    repo: '',
+    pull_number: 1,
+  }));
 }
 
 run();
