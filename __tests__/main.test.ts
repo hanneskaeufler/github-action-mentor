b6652be0fad7cd56206fcfd040159aec5b277b12
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-09-18 14:54:06 +0200

    Only comment with tags

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -7,4 +7,7 @@ describe('#run', () => {
 
   it('errors when the github webhook payload is not found', async () => {
   });
+
+  it('only comments with tips for the given tags', async () => {
+  });
 });
