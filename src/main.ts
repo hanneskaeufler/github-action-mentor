7d6f496343c68bbf27b81137a0a22a2840f4ed61
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-09-18 08:29:17 +0200

    Fix env fetching

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as github from '@actions/github';
 
 async function run() {
   const api = new github.GitHub(core.getInput('githubToken', { required: true }));
-  const event = core.getInput('githubEventFilepath', { required: true });
+  const event = process.env['GITHUB_EVENT_PATH'];
 
   console.log(event);
   console.log(api.pulls.get());
