96eaa8242c999a451039404ceadd8a5108858435
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-09-18 07:57:35 +0200

    Draft the first prototype

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 async function run() {
-  try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  const api = new github.GitHub(core.getInput('githubToken', { required: true }));
+  const event = core.getInput('githubEventFilepath', { required: true });
+
+  console.log(event);
+  console.log(api.pulls.get());
 }
 
 run();
