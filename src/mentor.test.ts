ef7b3144b8b6bace77f042b5e2c4945ed95f274c
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-09-19 12:44:19 +0200

    Run prettier on the codebase

--- src/mentor.test.ts
+++ src/mentor.test.ts
@@ -1,52 +1,53 @@
-import { default as mentor } from './mentor'
+import { default as mentor } from "./mentor"
 
 const mockApi = () => {
   return {
     issues: {
       createComment: jest.fn(),
-      listComments: jest.fn()
-    }
-  };
+      listComments: jest.fn(),
+    },
+  }
 }
 
-const sampleEvent = { number: 1337, repository: { name: 'repo', owner: { login: 'owner'} } };
+const sampleEvent = {
+  number: 1337,
+  repository: { name: "repo", owner: { login: "owner" } },
+}
 
-describe('#mentor', () => {
-  it('posts a comment to the github pr', async () => {
-    const api = mockApi();
-    api.issues.listComments.mockReturnValueOnce([]);
+describe("#mentor", () => {
+  it("posts a comment to the github pr", async () => {
+    const api = mockApi()
+    api.issues.listComments.mockReturnValueOnce([])
 
-    await mentor(api, sampleEvent);
+    await mentor(api, sampleEvent)
 
     expect(api.issues.createComment).toHaveBeenCalledWith({
-      owner: 'owner',
-      repo: 'repo',
+      owner: "owner",
+      repo: "repo",
       issue_number: 1337,
       body: expect.stringContaining("by mentor"),
-    });
-  });
+    })
+  })
 
-  it('doesnt comment a second time if there is already a mentor comment', async () => {
-    const api = mockApi();
-    api.issues.listComments.mockReturnValueOnce([{ body: 'by mentor', user: { login: 'github-actions' } }]);
+  it("doesnt comment a second time if there is already a mentor comment", async () => {
+    const api = mockApi()
+    api.issues.listComments.mockReturnValueOnce([{ body: "by mentor", user: { login: "github-actions" } }])
 
-    await mentor(api, sampleEvent);
+    await mentor(api, sampleEvent)
 
-    expect(api.issues.createComment).not.toHaveBeenCalled();
-  });
+    expect(api.issues.createComment).not.toHaveBeenCalled()
+  })
 
-  it('posts if none of the comment is by mentor', async () => {
-    const api = mockApi();
-    api.issues.listComments.mockReturnValueOnce([{ body: 'whatever', user: { login: 'someone' } }]);
+  it("posts if none of the comment is by mentor", async () => {
+    const api = mockApi()
+    api.issues.listComments.mockReturnValueOnce([{ body: "whatever", user: { login: "someone" } }])
 
-    await mentor(api, sampleEvent);
+    await mentor(api, sampleEvent)
 
-    expect(api.issues.createComment).toHaveBeenCalled();
-  });
+    expect(api.issues.createComment).toHaveBeenCalled()
+  })
 
-  it('errors when the github webhook payload is not found', async () => {
-  });
+  it("errors when the github webhook payload is not found", async () => {})
 
-  it('only comments with tips for the given tags', async () => {
-  });
-});
+  it("only comments with tips for the given tags", async () => {})
+})
--- src/mentor.ts
+++ src/mentor.ts
@@ -1,45 +1,45 @@
-const GITHUB_ACTIONS_USERNAME = 'github-actions';
+const GITHUB_ACTIONS_USERNAME = "github-actions"
 
 export default async function mentor(api, event) {
   if (await hasAlreadyCommented(api, event)) {
-    return;
+    return
   }
 
-  await postComment(api, event);
+  await postComment(api, event)
 }
 
 function issueParams(event) {
   return {
-    owner: event['repository']['owner']['login'],
-    repo: event['repository']['name'],
-    issue_number: event['number'],
+    owner: event["repository"]["owner"]["login"],
+    repo: event["repository"]["name"],
+    issue_number: event["number"],
   }
 }
 
 async function hasAlreadyCommented(api, event) {
-  const comments = await api.issues.listComments(issueParams(event));
+  const comments = await api.issues.listComments(issueParams(event))
 
-  return comments.length > 0 && oneOfCommentsIsByMentor(comments);
+  return comments.length > 0 && oneOfCommentsIsByMentor(comments)
 }
 
 function oneOfCommentsIsByMentor(comments) {
   for (let i = 0; i < comments.length; i++) {
-    const comment = comments[i];
-    console.log(comment);
+    const comment = comments[i]
+    console.log(comment)
 
     if (commentIsByMentor(comment)) {
-      return true;
+      return true
     }
   }
 }
 
 function commentIsByMentor(comment) {
-  return comment['user']['login'] == GITHUB_ACTIONS_USERNAME && /by mentor/.test(comment['body']);
+  return comment["user"]["login"] == GITHUB_ACTIONS_USERNAME && /by mentor/.test(comment["body"])
 }
 
 async function postComment(api, event) {
   await api.issues.createComment({
     ...issueParams(event),
-    body: 'Dude, a comment (by mentor)',
+    body: "Dude, a comment (by mentor)",
   })
 }
