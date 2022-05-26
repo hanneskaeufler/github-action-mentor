305b0b08ac4a413ce00db1d3f2aa756dd6d38452
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2022-05-26 21:40:03 +0200

    Perform @actions/github upgrade

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as fs from "fs"
 import { default as mentor } from "./mentor"
 
 async function run() {
-  const api = new github.GitHub(core.getInput("githubToken", { required: true }))
+  const api = github.getOctokit(core.getInput("githubToken", { required: true }))
 
   const event = JSON.parse(
     fs.readFileSync(process.env["GITHUB_EVENT_PATH"] || "", {
